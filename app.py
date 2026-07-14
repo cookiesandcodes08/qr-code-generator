23bb32d56e96ef269457d925d818f32693dd112b
Shreya Menon <shreya.menon2508@gmail.com>
2026-07-14 10:33:29 +0530

    Change theme to pink

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import qrcode
 
-st.title("QR Code Generator")
+st.title("QRify")
 
-data = st.text_input("Enter a URL or Text")
+data = st.text_input("Enter a URL or Text of a website :) ")
 
-if st.button("Generate QR Code"):
+if st.button("QRify!"): #used for button creation
     if data:
         img = qrcode.make(data)
 
@@ -13,7 +13,7 @@ if st.button("Generate QR Code"):
 
         st.image("QRcode.png")   # Display the saved image
 
-        st.success("QR Code Generated Successfully!")
+        st.success("QRify generated a QR for your website!")
 
     else:
         st.warning("Please enter some text first!")
