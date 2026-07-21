63e10f3b5aacfe02f330894fec3a146c002b0a1d
Shreya Menon <shreya.menon2508@gmail.com>
2026-07-21 19:32:44 +0530

    Add download button for QR codes

--- app.py
+++ app.py
@@ -15,5 +15,18 @@ if st.button("QRify!"): #used for button creation
 
         st.success("QRify generated a QR for your website!")
 
+  # Download button
+        with open("QRcode.png", "rb") as file:
+            st.download_button(
+                label="📥 Download QR Code",
+                data=file,
+                file_name="QRcode.png",
+                mime="image/png"
+            )
+
+        st.success("QRify generated a QR for your website!")
+
     else:
+        st.warning("Please enter some text first!")    
+else:
         st.warning("Please enter some text first!")
